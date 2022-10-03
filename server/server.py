1c0fcab369bf8c809675028899fffa6e25ad8f6b
Debjeet <debjeet194@gmail.com>
2022-10-04 03:28:12 +0530

    Modifying -- Modifying the UI in UI2

--- server/server.py
+++ server/server.py
@@ -1,16 +1,18 @@
 from flask import Flask, request, jsonify
 import util
 
+# from flask_cors import CORS
+
 app = Flask(__name__)
+# CORS(app)
 
 
-@app.route('/')
 @app.route('/classify_image', methods=['GET', 'POST'])
 def classify_image():
     image_data = request.form['image_data']
-
+    image_data = image_data.replace(" ", "")
+    print(image_data)
     response = jsonify(util.classify_image(image_data))
-
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
